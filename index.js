be26861416eaeccd902280b8f4bb6d90e267e6a0
Boi Schrader <boi1992@t-online.de>
2022-07-25 13:17:52 +0200

    credit card validation done wit solution help

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 function validateCreditCard(creditCardNum) {
+    const result = {valid :true , number : creditCardNum};
 
+    const ccNumberNoDash = creditCardNum.split("-").join("");
+    
+    if (ccNumberNoDash.length !== 16){
+        result.valid = false;
+        result.error = "wrong length, must be 16 digits";
+        return result;
+    }   
+    for (let i = 0 ; i < ccNumberNoDash.length;  i++){
+        let currentNumber = ccNumberNoDash[i];
+        currentNumber = parseInt(currentNumber);
+        
+        if (!Number.isInteger(currentNumber)){
+            result.valid = false;
+            result.error = "invalid characters";
+            return result
+        }
+    }
+    let twoDifferentDigits = false;
+    for (let i = 1; i < ccNumberNoDash.length; i++){
+        if (ccNumberNoDash[i] !== ccNumberNoDash[i - 1]) {
+          twoDifferentDigits = true;
+          break;
+        }
+      }
+        if (twoDifferentDigits === false) {
+    result.valid = false;
+    result.error = "_Need more than 1 unique digit_";
+    return result;
+  }
+  if (ccNumberNoDash[ccNumberNoDash.length - 1] % 2 !== 0) {
+    result.valid = false;
+    result.error = "_odd final number_";
+    return result;
+  }
+
+
+  let sum = 0;
+  for (let i = 0; i < ccNumberNoDash.length; i++) {
+    sum += parseInt(ccNumberNoDash[i]);
+  }
+  if (sum <= 16) {
+    result.valid = false;
+    result.error = "_sum less than 16_";
+    return result;
+  }
+
+  return result;
 };
 
 /**** tests *****/
